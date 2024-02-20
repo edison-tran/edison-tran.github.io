5dcefa22951b754437ffe9f346fa1af11aa38e43
edison-tran <edison.tran@torontomu.ca>
2024-02-19 20:22:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,5 @@ function promptName() {
     var userName = prompt("Please enter your name:");
     if (userName != null && userName != "") {
         document.getElementById("nameDisplay").innerText = "Your name is: " + userName;
-    } else {
-        document.getElementById("nameDisplay").innerText = "No name entered.";
-    }
+    } 
 }
